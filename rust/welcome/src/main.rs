66ea23cb9b8c97db48195a33ea6cd26f5ed32180
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-03-23 23:46:26 +0900

    Use big number

--- rust/welcome/src/main.rs
+++ rust/welcome/src/main.rs
@@ -4,4 +4,15 @@ fn main() {
     println!("length is {}", length);
 
     println!(r"\\\\\\\\\\");
+
+    //let big_number: i64;
+    //let big_number: i32;
+    let big_number;
+    //big_number = 2147483648;
+    big_number = 2147483648_i64;
+    println!("{}", big_number);
+
+    let avogadro_constant;
+    avogadro_constant = 6.02e+23;
+    println!("{}", avogadro_constant);
 }
