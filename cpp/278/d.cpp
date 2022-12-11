e0215d5023edf8732ae419b4cc58790acd2024d7
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-12-11 18:41:18 +0900

    Add 278-D

--- cpp/278/d.cpp
+++ cpp/278/d.cpp
@@ -8,14 +8,18 @@ int main()
   int n;
   cin >> n;
 
-  vector<long long> v(n);
+  map<int, long long> m;
   for (int i = 0; i < n; i++)
   {
-    cin >> v[i];
+    int a;
+    cin >> a;
+    m[i] = a;
   }
 
   int q;
   cin >> q;
+
+  int base = 0;
 
   for (int i = 0; i < q; i++)
   {
@@ -23,30 +27,21 @@ int main()
     cin >> type;
     if (type == 1)
     {
-      int value;
-      cin >> value;
-      for (int j = 0; j < n; j++)
-      {
-        v[j] = value;
-      }
+      cin >> base;
+      m.clear();
     }
     if (type == 2)
     {
       int index, add;
       cin >> index >> add;
-      v[index - 1] += add;
+      m[index - 1] += add;
     }
     if (type == 3)
     {
       int index;
       cin >> index;
-      cout << v[index - 1] << endl;
+      cout << base + m[index - 1] << endl;
     }
-    // for (int j = 0; j < n; j++)
-    // {
-    //   cout << v[j] << ",";
-    // }
-    // cout << endl;
   }
   return 0;
 }
