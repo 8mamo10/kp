289c031e0ab3b2208352a3802e2fea2ac9664c84
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-12-31 22:27:09 +0900

    Add 280-d

--- cpp/280/d.cpp
+++ cpp/280/d.cpp
@@ -3,19 +3,67 @@ using namespace std;
 
 using ll = long long;
 
+ll f(ll n, ll p)
+{
+  if (n == 0)
+  {
+    return 0;
+  }
+  n /= p;
+  return n + f(n, p);
+}
+
 int main()
 {
   ll k;
   cin >> k;
-  ll total = 1;
-  for (ll n = 1; n < k; n++)
+  vector<pair<ll, int>> ps;
   {
-    total *= n;
-    if (total % k == 0)
+    ll x = k;
+    for (ll i = 2; i * i <= x; i++)
     {
-      cout << n << endl;
-      return 0;
+      if (x % i)
+      {
+        continue;
+      }
+      int cnt = 0;
+      while (x % i == 0)
+      {
+        x /= i;
+        cnt++;
+      }
+      ps.emplace_back(i, cnt);
+    }
+    if (x != 1)
+    {
+      ps.emplace_back(x, 1);
     }
   }
+
+  // for (auto itr = ps.begin(); itr != ps.end(); itr++)
+  // {
+  //   cout << itr->first << ":" << itr->second << endl;
+  // }
+
+  ll ac = k, wa = 0;
+  while (ac - wa > 1)
+  {
+    ll wj = (ac + wa) / 2;
+    bool ok = true;
+    for (auto [p, cnt] : ps)
+    {
+      if (f(wj, p) < cnt)
+        ok = false;
+    }
+    if (ok)
+    {
+      ac = wj;
+    }
+    else
+    {
+      wa = wj;
+    }
+  }
+  cout << ac << endl;
   return 0;
 }
