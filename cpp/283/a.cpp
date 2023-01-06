e02f45bb888d250aa54ccfb6dc0671d240fda775
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-01-06 23:40:08 +0900

    Add 283-a

---
+++ cpp/283/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  int ans = 1;
+  for (int i = 0; i < b; i++)
+  {
+    ans *= a;
+  }
+  cout << ans << endl;
+  return 0;
+}
