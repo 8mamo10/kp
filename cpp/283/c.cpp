9dc1b2f12f37851e270441ed1e86f4fb732e4e0a
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-01-17 23:57:32 +0700

    Add 283-c

--- cpp/283/c.cpp
+++ cpp/283/c.cpp
@@ -7,5 +7,13 @@ int main()
 {
   string s;
   cin >> s;
+  int count = 0;
+  size_t pos = s.find("00");
+  while (pos != std::string::npos)
+  {
+    count++;
+    pos = s.find("00", pos + 2);
+  }
+  cout << s.length() - count << endl;
   return 0;
 }
