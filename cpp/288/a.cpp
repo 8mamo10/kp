d21f5a224a4116375d686719f49c2e14d8d1d5d4
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-02-18 19:04:26 +0700

    Add 288-a

---
+++ cpp/288/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+  int N;
+  cin >> N;
+  for (int i = 0; i < N; i++)
+  {
+    ll A, B;
+    cin >> A >> B;
+    cout << A + B << endl;
+  }
+  return 0;
+}
