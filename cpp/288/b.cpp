b8f0bdaeab79886cc7de8643b603d01356af2c31
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-02-19 23:57:15 +0700

    Add 288-b

---
+++ cpp/288/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+  int N, K;
+  cin >> N >> K;
+  vector<string> v;
+  for (int i = 0; i < N; i++)
+  {
+    string S;
+    cin >> S;
+    v.push_back(S);
+  }
+  vector<string> sub_v = {v.begin(), v.begin() + K};
+
+  sort(sub_v.begin(), sub_v.end());
+  for (int i = 0; i < K; i++)
+  {
+    cout << sub_v[i] << endl;
+  }
+  return 0;
+}
