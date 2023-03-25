3201965ecbc11d628e6bcdc9c0fed4ea028bace0
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-03-25 18:49:56 +0900

    Add 291-c, WIP

---
+++ cpp/291/c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dump(vector<int> v)
+{
+  for (auto itr : v)
+  {
+    cout << itr << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+
+  return 0;
+}
