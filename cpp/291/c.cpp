3f27e1e65f8a257ab95bd19277d859f3128a636d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-03-26 10:13:56 +0900

    Add 291-c

--- cpp/291/c.cpp
+++ cpp/291/c.cpp
@@ -17,5 +17,41 @@ int main()
   string S;
   cin >> S;
 
+  map<pair<int, int>, bool> m;
+  pair<int, int> current = make_pair(0, 0);
+  m[current] = true;
+  for (int i = 0; i < N; i++)
+  {
+    // cout << "current=(" << current.first << "," << current.second << ")" << endl;
+
+    int c = S[i];
+    pair<int, int> next;
+    if (c == 'R')
+    {
+      next = make_pair(current.first + 1, current.second);
+    }
+    else if (c == 'L')
+    {
+      next = make_pair(current.first - 1, current.second);
+    }
+    else if (c == 'U')
+    {
+      next = make_pair(current.first, current.second + 1);
+    }
+    else if (c == 'D')
+    {
+      next = make_pair(current.first, current.second - 1);
+    }
+    // cout << "next=(" << next.first << "," << next.second << ")" << endl;
+
+    if (m[next])
+    {
+      cout << "Yes" << endl;
+      return 0;
+    }
+    m[next] = true;
+    current = next;
+  }
+  cout << "No" << endl;
   return 0;
 }
