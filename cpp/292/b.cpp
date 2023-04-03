e2e518ae94e1c0f3948ae00616753d9823282134
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-04-03 23:28:04 +0900

    Add 292-b, WIP

---
+++ cpp/292/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> events(Q);
+  for (int i = 0; i < Q; i++)
+  {
+    int type, player;
+    cin >> type >> player;
+  }
+  return 0;
+}
