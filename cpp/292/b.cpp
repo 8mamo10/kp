6ea89e17b0e711fa61597233a03cb7bb92e134ba
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-04-04 17:45:50 +0900

    Add 292-b

--- cpp/292/b.cpp
+++ cpp/292/b.cpp
@@ -5,11 +5,31 @@ int main()
 {
   int N, Q;
   cin >> N >> Q;
-  vector<int> events(Q);
+  vector<int> players(N, 0);
   for (int i = 0; i < Q; i++)
   {
-    int type, player;
-    cin >> type >> player;
+    int e, x;
+    cin >> e >> x;
+    x--;
+    switch (e)
+    {
+    case 1:
+      players[x] += 1;
+      break;
+    case 2:
+      players[x] += 2;
+      break;
+    case 3:
+      if (players[x] >= 2)
+      {
+        cout << "Yes" << endl;
+      }
+      else
+      {
+        cout << "No" << endl;
+      }
+      break;
+    }
   }
   return 0;
 }
