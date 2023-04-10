2d492b997bd4e6cb1672e0344a22f3ff6de69c71
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-04-11 00:11:23 +0700

    Add 293-b

--- cpp/293/b.cpp
+++ cpp/293/b.cpp
@@ -10,5 +10,22 @@ int main()
   {
     cin >> V[i];
   }
+  vector<bool> called(N, false);
+  for (int i = 0; i < N; i++)
+  {
+    if (!called[i])
+    {
+      called[V[i] - 1] = true;
+    }
+  }
+  cout << count(called.begin(), called.end(), false) << endl;
+  ;
+  for (int i = 0; i < N; i++)
+  {
+    if (called[i])
+      continue;
+    cout << i + 1 << " ";
+  }
+  cout << endl;
   return 0;
 }
