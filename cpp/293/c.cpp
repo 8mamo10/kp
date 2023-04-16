dfd5238760ae7c5fddba71f0706a57d6bed3985a
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-04-17 00:28:37 +0700

    Add 293-c

--- cpp/293/c.cpp
+++ cpp/293/c.cpp
@@ -1,20 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int H, W, ans;
+vector<vector<int>> A;
+
+void dfs(int i, int j, set<int> s)
+{
+  if (s.count(A[i][j]))
+  {
+    return;
+  }
+  s.insert(A[i][j]);
+  if (i == H - 1 && j == W - 1)
+  {
+    ans++;
+    return;
+  }
+  if (j + 1 < W)
+  {
+    dfs(i, j + 1, s);
+  }
+  if (i + 1 < H)
+  {
+    dfs(i + 1, j, s);
+  }
+}
+
 int main()
 {
-  int H, W;
   cin >> H >> W;
-  vector<vector<int>> V;
+  A = vector(H, vector<int>(W));
   for (int i = 0; i < H; i++)
   {
     for (int j = 0; j < W; j++)
     {
-      int A;
-      cin >> A;
-      V[i][j] = A;
+      cin >> A[i][j];
     }
   }
-
+  dfs(0, 0, set<int>());
+  cout << ans << endl;
   return 0;
 }
