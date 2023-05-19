0f110693c335438b025f18fb60083957ce65da43
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-05-19 23:58:29 +0700

    Add 298-c, WIP

---
+++ cpp/298/c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int N, Q;
+  cin >> N >> Q;
+
+  for (int _ = 0; _ < Q; _++)
+  {
+    int type;
+    cin >> type;
+    int i, j;
+    if (type == 1)
+    {
+      cin >> i >> j;
+    }
+    else if (type == 2)
+    {
+      cin >> i;
+    }
+    else if (type == 3)
+    {
+      cin >> i;
+    }
+  }
+  return 0;
+}
