eea0c9405e23ddb7f66e6b53fb84c39450615d20
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-05-21 19:58:16 +0700

    Add 298-c, TLE

--- cpp/298/c.cpp
+++ cpp/298/c.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int N, Q;
   cin >> N >> Q;
+
+  map<int, vector<int>> box;
+  map<int, set<int>> number;
 
   for (int _ = 0; _ < Q; _++)
   {
@@ -14,15 +20,31 @@ int main()
     if (type == 1)
     {
       cin >> i >> j;
+      box[j].push_back(i);
+      sort(box[j].begin(), box[j].end());
+      number[i].insert(j);
     }
     else if (type == 2)
     {
       cin >> i;
+
+      for (auto itr : box[i])
+      {
+        cout << itr << " ";
+      }
+      cout << endl;
     }
     else if (type == 3)
     {
       cin >> i;
+
+      for (auto itr : number[i])
+      {
+        cout << itr << " ";
+      }
+      cout << endl;
     }
   }
+
   return 0;
 }
