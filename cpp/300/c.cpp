1813ad43496c9aadbe1cb94c812c173d33b5cd48
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-05-31 23:46:46 +0700

    Add 300-c, WIP

---
+++ cpp/300/c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+int main()
+{
+  // ios::sync_with_stdio(false);
+  // cin.tie(nullptr);
+
+  int H, W;
+  cin >> H >> W;
+  vector<string> C(H);
+  for (int i = 0; i < H; i <<)
+  {
+    cin >> C[i];
+  }
+  return 0;
+}
