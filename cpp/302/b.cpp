3d2888269e0d1f9b45a9dd007b2159b6dc8cdd0a
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-06-14 23:21:14 +0700

    Add 302-b, WIP

---
+++ cpp/302/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ull = unsigned long long;
+
+int main()
+{
+  int H, W;
+  cin >> H >> W;
+  vector<string> S(H);
+  for (int i = 0; i < H; i++)
+  {
+    cin >> S[i];
+  }
+
+  return 0;
+}
