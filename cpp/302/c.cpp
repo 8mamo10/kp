1485ae806d758689df4c2200791f71e8655d1ddc
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-06-26 17:31:05 +0700

    Add 302-c

--- cpp/302/c.cpp
+++ cpp/302/c.cpp
@@ -9,25 +9,41 @@ using namespace std;
 
 int main()
 {
-  // int N, M;
-  // cin >> N >> M;
-  // vector<string> S(N);
-  // for (int i = 0; i < N; i++)
-  // {
-  //   cin >> S[i];
-  // }
+  int N, M;
+  cin >> N >> M;
+  vector<string> S(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> S[i];
+  }
 
-  vector<string> S;
-  S.push_back("aaa");
-  S.push_back("bbb");
-  S.push_back("ccc");
+  sort(S.begin(), S.end());
+
   do
   {
-    for (auto itr : S)
+    bool ok = true;
+    for (int i = 0; i < N - 1; i++)
     {
-      cout << itr << " ";
+      int count = 0;
+      for (int j = 0; j < M; j++)
+      {
+        if (S[i][j] != S[i + 1][j])
+        {
+          count++;
+        }
+        if (count > 1)
+        {
+          ok = false;
+        }
+      }
     }
-    cout << endl;
+    if (ok)
+    {
+      cout << "Yes" << endl;
+      return 0;
+    }
   } while (next_permutation(S.begin(), S.end()));
+  cout << "No" << endl;
+
   return 0;
 }
