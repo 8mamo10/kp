5d5a05b3afc8b5cf626adfdd1c85ec02119e858d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-06-28 23:49:48 +0700

    Add 302-d, WIP

---
+++ cpp/302/d.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+using ll = long long;
+// using ull = unsigned long long;
+
+int main()
+{
+  int N, M, D;
+  cin >> N >> M >> D;
+  vector<ll> A(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> A[i];
+  }
+  vector<ll> B(M);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> B[i];
+  }
+  return 0;
+}
