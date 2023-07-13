4eb05eef942fee9a969195e4dd40556d74f9a667
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-07-13 23:49:24 +0700

    Add 304-c, WIP

---
+++ cpp/304/c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N, D;
+  cin >> N >> D;
+  set<pair<int, int>> st;
+  for (int i = 0; i < N; i++)
+  {
+    int X, Y;
+    cin >> X >> Y;
+    st.insert({X, Y});
+  }
+
+  return 0;
+}
