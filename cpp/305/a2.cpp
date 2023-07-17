c53c0025599ed79e1d9a99ac43632880643472ee
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-07-17 14:55:25 +0700

    Add 305-a2

---
+++ cpp/305/a2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  cout << round(N / 5.0) * 5 << endl;
+}
