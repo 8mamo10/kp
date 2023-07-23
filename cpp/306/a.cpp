a7e5f72bbe3978b930857e8ec6fa98f503eb689b
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-07-23 18:00:56 +0700

    Add 306-a

---
+++ cpp/306/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+
+  for (int i = 0; i < N; i++)
+  {
+    cout << S[i] << S[i];
+  }
+  cout << endl;
+  return 0;
+}
