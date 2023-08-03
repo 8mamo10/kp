90b918fdda224d4e79368aad7263d6624b18fe42
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-08-03 18:55:48 +0700

    Add 308-c, WA

---
+++ cpp/308/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N;
+  cin >> N;
+  map<double, vector<int>, std::greater<double>> results;
+  for (int i = 0; i < N; i++)
+  {
+    double A, B;
+    cin >> A >> B;
+    double d = A / (A + B);
+    // cout << d << endl;
+    results[d].push_back(i + 1);
+  }
+  for (auto itr : results)
+  {
+    for (auto itr2 : itr.second)
+    {
+      cout << itr2 << " ";
+    }
+  }
+  cout << endl;
+  return 0;
+}
