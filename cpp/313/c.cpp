c30ca21e4812b7c4f2dc9fd74db62cd5f89a71ec
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-08-25 00:10:56 +0900

    Add 313-c, WIP

--- cpp/313/c.cpp
+++ cpp/313/c.cpp
@@ -8,6 +8,12 @@ using namespace std;
 // using ll = long long;
 // using ull = unsigned long long;
 // long long INF = 1000000000000000000;
+bool finished(vector<int> A)
+{
+  auto mx = max_element(A.begin(), A.end());
+  auto mn = min_element(A.begin(), A.end());
+  return (*mx - *mn <= 1);
+}
 
 int main()
 {
@@ -18,5 +24,16 @@ int main()
   {
     cin >> A[i];
   }
+
+  int count = 0;
+  while (!finished(A))
+  {
+    auto mx = max_element(A.begin(), A.end());
+    (*mx)--;
+    auto mn = min_element(A.begin(), A.end());
+    (*mn)++;
+    count++;
+  }
+  cout << count << endl;
   return 0;
 }
