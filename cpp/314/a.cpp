5404c0229d14232e7ad3ed64ddadae323bf0a7cd
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-08-28 22:46:38 +0900

    Add 314-a

---
+++ cpp/314/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  string pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+  int N;
+  cin >> N;
+  cout << pi.substr(0, N + 2) << endl;
+  return 0;
+}
