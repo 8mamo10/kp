e3f36e8cdea22e579753ff75ab1173bbf56d3d00
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-09-16 23:32:56 +0700

    Add 317-c, WIP

---
+++ cpp/317/c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  return 0;
+}
