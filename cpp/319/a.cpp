0908d41a08576282aed1841a9994dd679b118ab2
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-09-24 19:41:19 +0700

    Add 319-a

---
+++ cpp/319/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  map<string, int> mp = {
+      {"tourist", 3858},
+      {"ksun48", 3679},
+      {"Benq", 3658},
+      {"Um_nik", 3648},
+      {"apiad", 3638},
+      {"Stonefeang", 3630},
+      {"ecnerwala", 3613},
+      {"mnbvmar", 3555},
+      {"newbiedmy", 3516},
+      {"semiexp", 3481},
+  };
+  string s;
+  cin >> s;
+  cout << mp[s] << endl;
+
+  return 0;
+}
