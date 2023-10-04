1a3c67daa96989866658852db5a6784a78c14d60
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-10-04 23:52:26 +0700

    Add 320-c, WIP

---
+++ cpp/320/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int m;
+  cin >> m;
+  vector<string> s(m);
+  for (int i = 0; i < 3; i++)
+  {
+    cin >> s[i];
+  }
+  return 0;
+}
