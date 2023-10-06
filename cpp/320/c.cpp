64c8b23ea2a2ba50a28f6f8568d4179583913cc2
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-10-06 19:36:30 +0900

    Add 320-c

--- cpp/320/c.cpp
+++ cpp/320/c.cpp
@@ -13,20 +13,58 @@ int main()
 {
   int m;
   cin >> m;
-  vector<string> s(m);
+  vector<string> s(3);
   for (int i = 0; i < 3; i++)
   {
     cin >> s[i];
   }
+  sort(s.begin(), s.end());
 
-  vector<int> a = {1, 2, 3};
-  int count = 0;
+  // cout << s[0] << endl;
+  // int p = s[0].find('1');
+  // cout << p << endl;
+  // p = s[0].find('8');
+  // cout << p << endl;
+  // p = s[0].find('8', 5);
+  // cout << p << endl;
+  // return -1;
+
+  int ans = INT_MAX;
   do
   {
-    count++;
-    cout << "---" << count << "---" << endl;
-    cout << a[0] << a[1] << a[2] << endl;
-  } while (next_permutation(a.begin(), a.end()));
+    for (int i = 0; i <= 9; i++)
+    {
+      int t = -1;
+      for (auto itr : s)
+      {
+        t++;
+        size_t pos = itr.find(to_string(i), t % m);
+        if (pos != string::npos)
+        {
+          t += (pos - (t % m));
+          continue;
+        }
+        t += (m - (t % m));
+        pos = itr.find(to_string(i));
+        if (pos != string::npos)
+        {
+          t += pos;
+          continue;
+        }
+        t = INT_MAX;
+        break;
+      }
+      ans = min(ans, t);
+    }
+  } while (next_permutation(s.begin(), s.end()));
 
+  if (ans == INT_MAX)
+  {
+    cout << -1 << endl;
+  }
+  else
+  {
+    cout << ans << endl;
+  }
   return 0;
 }
