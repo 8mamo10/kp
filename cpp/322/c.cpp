2231e0aa0917381a4c690b3cfeed02196e0befc7
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-10-21 19:24:12 +0700

    Add 322-c

--- cpp/322/c.cpp
+++ cpp/322/c.cpp
@@ -18,5 +18,34 @@ int main()
   {
     cin >> a[i];
   }
+
+  vector<bool> s(n, false);
+  for (int i = 0; i < m; i++)
+  {
+    s[a[i] - 1] = true;
+  }
+
+  // for (auto itr : s)
+  // {
+  //   cout << itr << ",";
+  // }
+  // cout << endl;
+
+  int w = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (s[i])
+    {
+      for (int j = 0; j <= w; j++)
+      {
+        cout << w - j << endl;
+      }
+      w = 0;
+    }
+    else
+    {
+      w++;
+    }
+  }
   return 0;
 }
