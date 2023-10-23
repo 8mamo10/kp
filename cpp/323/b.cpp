03032064f1b6bacc07243cd0814f459322267a79
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-10-23 23:56:31 +0700

    Add 323-b, WIP

---
+++ cpp/323/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> S[i];
+  }
+  return 0;
+}
