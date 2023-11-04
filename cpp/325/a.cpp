1796368af5f9e3839c1c301684de533e090b713e
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-11-04 23:50:20 +0700

    Add 325-a

---
+++ cpp/325/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  string S, T;
+  cin >> S >> T;
+  cout << S << " san" << endl;
+  return 0;
+}
