91418d69e98bb2634c46416818ecf0c8d8107a56
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-11-09 23:58:49 +0700

    Add 326-b, WIP

---
+++ cpp/326/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  string N;
+  cin >> N;
+
+  return 0;
+}
