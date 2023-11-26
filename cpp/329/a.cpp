4ada88cd951a7b8467d1cb828a6e6820b81b10b3
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-11-26 21:33:03 +0900

    Add 329-a

---
+++ cpp/329/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  string S;
+  cin >> S;
+  for (auto c : S)
+  {
+    cout << c << " ";
+  }
+  cout << endl;
+  return 0;
+}
