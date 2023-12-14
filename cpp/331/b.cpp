3e672e4efbdb271bd951dcc19433a758b65f48a1
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-12-14 23:43:49 +0700

    Add 331-b

--- cpp/331/b.cpp
+++ cpp/331/b.cpp
@@ -14,5 +14,24 @@ int main()
 {
   int N, S, M, L;
   cin >> N >> S >> M >> L;
+  int ans = INT_MAX;
+
+  for (int i = 0; i <= 100; i++)
+  {
+    for (int j = 0; j <= 100; j++)
+    {
+      for (int k = 0; k <= 100; k++)
+      {
+        int num = i * 6 + j * 8 + k * 12;
+        if (num < N)
+        {
+          continue;
+        }
+        int bill = i * S + j * M + k * L;
+        ans = min(ans, bill);
+      }
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
