ef68c2d5a3ec2f2f193ca14b4fbb86bdf93b173a
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-12-17 16:26:15 +0700

    Add 331-c, TLE

--- cpp/331/c.cpp
+++ cpp/331/c.cpp
@@ -6,12 +6,22 @@ using namespace std;
 // using namespace atcoder;
 // using mint = modint998244353;
 
-// using ll = long long;
+using ll = long long;
 // using ull = unsigned long long;
 // long long INF = 1000000000000000000;
 
 int main()
 {
+  // map<int, int> mp;
+  // mp.insert({1, 111});
+  // mp.insert({3, 333});
+  // mp.insert({2, 222});
+  // for (auto itr : mp)
+  // {
+  //   cout << itr.first << ":" << itr.second << endl;
+  // }
+  // return 0;
+
   int N;
   cin >> N;
   vector<int> A(N);
@@ -19,5 +29,33 @@ int main()
   {
     cin >> A[i];
   }
+
+  map<int, ll> mp;
+  for (int i = 0; i < N; i++)
+  {
+    int v = A[i];
+    if (mp.find(v) == mp.end())
+    {
+      mp.insert({v, v});
+      continue;
+    }
+    mp[v] += v;
+  }
+
+  for (int i = 0; i < N; i++)
+  {
+    int v = A[i];
+    ll ans = 0;
+    for (auto itr : mp)
+    {
+      if (itr.first <= v)
+      {
+        continue;
+      }
+      ans += itr.second;
+    }
+    cout << ans << " ";
+  }
+  cout << endl;
   return 0;
 }
