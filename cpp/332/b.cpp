76c7366aabe3886f0067f4979f2a753fa77e5d22
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2023-12-21 23:56:29 +0700

    Add 332-b, WIP

---
+++ cpp/332/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int K, G, M;
+  cin >> K >> G >> M;
+  return 0;
+}
