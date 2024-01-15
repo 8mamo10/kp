8fe32afd4d0da7a4c3ae230fde061e1bcffa0138
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-01-15 09:46:13 +0700

    Add 334-a

---
+++ cpp/334/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int B, G;
+  cin >> B >> G;
+  if (B > G)
+  {
+    cout << "Bat" << endl;
+  }
+  else
+  {
+    cout << "Glove" << endl;
+  }
+  return 0;
+}
