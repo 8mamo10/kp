6b4f76dd3915df5031aa31a99b9e24cbc7df70cc
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-01-17 18:13:34 +0700

    Add 334-b

--- cpp/334/b.cpp
+++ cpp/334/b.cpp
@@ -10,9 +10,17 @@ using ll = long long;
 // using ull = unsigned long long;
 // long long INF = 1000000000000000000;
 
+ll floor(ll x, ll m)
+{
+  ll r = (x % m + m) % m;
+  return (x - r) / m;
+}
+
 int main()
 {
   ll A, M, L, R;
   cin >> A >> M >> L >> R;
+  L -= A, R -= A;
+  cout << floor(R, M) - floor(L - 1, M) << endl;
   return 0;
 }
