e6d4e3c03eca74c2654ece6ed7c2f7d1f17b26ee
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-01-25 23:59:24 +0700

    Add 334-c, WIP

---
+++ cpp/334/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N, K;
+  cin << N << K;
+  vector<int> A(K);
+  for (int i = 0; i < K; i++)
+  {
+    cin << A[i];
+  }
+  return 0;
+}
