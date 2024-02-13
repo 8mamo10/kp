856377dbf6f0b2d04beb9abfa9975d96b1431308
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-02-13 23:53:55 +0900

    Add 336-a

---
+++ cpp/336/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N;
+  cin >> N;
+  cout << "L";
+  for (int i = 0; i < N; i++)
+  {
+    cout << "o";
+  }
+  cout << "ng" << endl;
+  ;
+  return 0;
+}
