ccf2f39ae47522bcf48b535ac647203c5586c727
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-02-22 07:47:53 +0700

    Add 336-c

--- cpp/336/c.cpp
+++ cpp/336/c.cpp
@@ -8,31 +8,30 @@ using namespace std;
 
 using ll = long long;
 // using ull = unsigned long long;
-long long INF = 1000000000000000000;
+// long long INF = 1000000000000000000;
 // long long INF = 20;
 
 int main()
 {
   ll N;
   cin >> N;
-  ll count = 0;
-  for (ll i = 0; i < INF; i += 2)
+  N--;
+
+  vector<int> a;
+  while (N)
   {
-    string s = to_string(i);
-    if (s.find('1') != string::npos ||
-        s.find('3') != string::npos ||
-        s.find('5') != string::npos ||
-        s.find('7') != string::npos ||
-        s.find('9') != string::npos)
-    {
-      continue;
-    }
-    count++;
-    if (count == N)
-    {
-      cout << i << endl;
-      return 0;
-    }
+    a.push_back(N % 5);
+    N /= 5;
   }
+  if (a.empty())
+  {
+    a.push_back(0);
+  }
+  reverse(begin(a), end(a));
+  for (auto &x : a)
+  {
+    cout << x * 2;
+  };
+  cout << endl;
   return 0;
 }
