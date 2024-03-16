1d18682472cdecf4bff3ecb60253d54a667d5ab8
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-03-16 17:26:40 +0900

    Add 338-c

--- cpp/338/c.cpp
+++ cpp/338/c.cpp
@@ -6,12 +6,61 @@ using namespace std;
 // using namespace atcoder;
 // using mint = modint998244353;
 
-using ll = long long;
+// using ll = long long;
 // using ull = unsigned long long;
 // long long INF = 1000000000000000000;
 // long long INF = 20;
 
 int main()
 {
+  int N;
+  cin >> N;
+  vector<int> Q(N), A(N), B(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> Q[i];
+  }
+  for (int i = 0; i < N; i++)
+  {
+    cin >> A[i];
+  }
+
+  for (int i = 0; i < N; i++)
+  {
+    cin >> B[i];
+  }
+
+  int ans = 0;
+  for (int x = 0;; x++)
+  {
+    vector<int> r(N);
+    for (int i = 0; i < N; i++)
+    {
+      r[i] = Q[i] - A[i] * x;
+    }
+    bool ok = true;
+    for (int i = 0; i < N; i++)
+    {
+      if (r[i] < 0)
+      {
+        ok = false;
+      }
+    }
+    if (!ok)
+    {
+      break;
+    }
+    int y = INT_MAX;
+    for (int i = 0; i < N; i++)
+    {
+      if (B[i] == 0)
+      {
+        continue;
+      }
+      y = min(y, r[i] / B[i]);
+    }
+    ans = max(ans, x + y);
+  }
+  cout << ans << endl;
   return 0;
 }
