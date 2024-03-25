b1b61a77d4410d085e0805f51cc45a60ec6a2048
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-03-25 23:57:46 +0900

    Add 339-b, WIP

---
+++ cpp/339/b.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+// long long INF = 20;
+
+const char WHITE = '.';
+const char BLACK = '#';
+
+int main()
+{
+  int H, W, N;
+  cin >> H >> W >> N;
+
+  vector<vector<char>> grid(H, vector<char>(W, WHITE));
+
+  for (auto itr : grid)
+  {
+    printv(itr, false);
+  }
+
+  pair<int, int> current = {0, 0};
+  pair<int, int> direction = {0, 1};
+
+  for (int i = 0; i < N; i++)
+  {
+    char color = grid[current.second][current.first];
+    if (color == WHITE)
+    {
+    }
+    else
+    {
+    }
+  }
+  return 0;
+}
