b89b05340a45bfaafd980a44145140a8535c9020
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-04-19 09:19:48 +0700

    Add 341-a

---
+++ cpp/341/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int N;
+  cin >> N;
+  for (int i = 0; i < N; i++)
+  {
+    cout << 1 << 0;
+  }
+  cout << 1 << endl;
+  return 0;
+}
