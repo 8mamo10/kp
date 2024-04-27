63b1c12aa0db34f0f9a9a9caf8e5b1aa52878732
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-04-27 21:39:43 +0700

    Add 341-c

--- cpp/341/c.cpp
+++ cpp/341/c.cpp
@@ -10,6 +10,9 @@ using namespace std;
 // using ull = unsigned long long;
 // long long INF = 1000000000000000000;
 
+#define RIKU '.'
+#define UMI '#'
+
 int main()
 {
   int h, w, n;
@@ -21,9 +24,51 @@ int main()
   {
     cin >> s[i];
   }
-  for (auto itr : s)
+
+  set<pair<int, int>> st;
+  for (int i = 0; i < h; i++)
   {
-    cout << itr << endl;
+    for (int j = 0; j < w; j++)
+    {
+      int y = i;
+      int x = j;
+      bool ok = true;
+      if (s[y][x] == UMI)
+      {
+        ok = false;
+        continue;
+      }
+      for (int k = 0; k < n; k++)
+      {
+        char c = t[k];
+        if (c == 'L')
+        {
+          x -= 1;
+        }
+        else if (c == 'R')
+        {
+          x += 1;
+        }
+        else if (c == 'U')
+        {
+          y -= 1;
+        }
+        else if (c == 'D')
+        {
+          y += 1;
+        }
+        if (s[y][x] == UMI)
+        {
+          ok = false;
+          break;
+        }
+      }
+      if (ok)
+      {
+        st.insert({x, y});
+      }
+    }
   }
+  cout << st.size() << endl;
   return 0;
 }
