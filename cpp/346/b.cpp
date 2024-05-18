5ac55e31483ff92939b1774f801505585c4f0934
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-05-18 23:56:04 +0700

    Add 346-b, WIP

---
+++ cpp/346/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int w, b;
+  cin >> w >> b;
+  int n = w + b;
+  string s = "wbwbwwbwbwbw";
+  size_t m = s.size();
+  for (int i = 0; i < m; i++)
+  {
+    int nw = 0, nb = 0;
+  }
+  return 0;
+}
