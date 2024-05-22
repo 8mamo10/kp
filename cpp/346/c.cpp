d697ac8a0e1dc452c57696479b8221eed6d9abff
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-05-22 23:56:53 +0700

    Add 346-c, WIP

---
+++ cpp/346/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int n, k;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  return 0;
+}
