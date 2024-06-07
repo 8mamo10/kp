980ff319e18a20b86a414bc4251d54b96cfea949
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-06-07 23:57:51 +0900

    Add 348-b, WIP

---
+++ cpp/348/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+// #include "my.h"
+using namespace std;
+
+// #include <atcoder/modint>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+// using ll = long long;
+// using ull = unsigned long long;
+// long long INF = 1000000000000000000;
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> x(n), y(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> x[i] >> y[i];
+  }
+
+  return 0;
+}
