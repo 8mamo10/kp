e91d58b8d07785ba0d8d458f26bf56c79c391f75
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-05-30 15:20:42 +0700

    404A

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -1,48 +1,20 @@
-use std::{collections::HashSet, vec};
+use std::collections::HashSet;
 
 fn main() {
     proconio::input! {
-        n: usize,
-        _m: usize,
-        q: usize,
+        s: String,
     }
 
-    let mut all_access_granted: Vec<bool> = vec![false; n + 1];
-    let mut spedific_access_granted: Vec<HashSet<usize>> = vec![HashSet::new(); n + 1];
+    let mut s_chars: HashSet<char> = HashSet::new();
+    for c in s.chars() {
+        s_chars.insert(c);
+    }
 
-    for _ in 0..q {
-        proconio::input! {
-            query_type: u8,
-        }
-
-        match query_type {
-            1 => {
-                proconio::input! {
-                    x: usize,
-                    y: usize
-                }
-                if !all_access_granted[x] {
-                    spedific_access_granted[x].insert(y);
-                }
-            }
-            2 => {
-                proconio::input! {
-                    x: usize
-                }
-                all_access_granted[x] = true;
-            }
-            3 => {
-                proconio::input! {
-                    x: usize,
-                    y: usize,
-                }
-                if all_access_granted[x] || spedific_access_granted[x].contains(&y) {
-                    println!("Yes");
-                } else {
-                    println!("No");
-                }
-            }
-            _ => unreachable!(),
+    let alphabet: &str = "abcdefghijklmnopqrstuvwxyz";
+    for a in alphabet.chars() {
+        if !s_chars.contains(&a) {
+            println!("{}", a);
+            return;
         }
     }
 }
