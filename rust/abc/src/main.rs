4f944d5ecbe676021245d50d737db6f192bd5f07
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-06-13 21:17:55 +0900

    407A

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -1,48 +1,13 @@
-use std::cmp::min;
+use proconio::input;
 
 fn main() {
-    proconio::input! {
-        n: usize,
+    input! {
+        a: i32,
+        b: i32,
     }
 
-    let mut s: Vec<Vec<char>> = Vec::with_capacity(n);
-    for _ in 0..n {
-        proconio::input! {
-            row: String,
-        }
-        s.push(row.chars().collect());
-    }
+    let result: f32 = a as f32 / b as f32;
+    let round = (result + 0.5).floor() as i32;
 
-    let mut t: Vec<Vec<char>> = Vec::with_capacity(n);
-    for _ in 0..n {
-        proconio::input! {
-            row: String,
-        }
-        t.push(row.chars().collect());
-    }
-
-    let mut min_ops = std::usize::MAX;
-    let mut current_s = s;
-
-    for op in 0..4 {
-        let mut diff_count = 0;
-        for i in 0..n {
-            for j in 0..n {
-                if current_s[i][j] != t[i][j] {
-                    diff_count += 1;
-                }
-            }
-        }
-
-        min_ops = min(min_ops, diff_count + op);
-
-        let mut next_s = vec![vec![' '; n]; n];
-        for i in 0..n {
-            for j in 0..n {
-                next_s[j][n - 1 - i] = current_s[i][j];
-            }
-        }
-        current_s = next_s;
-    }
-    println!("{}", min_ops);
+    println!("{}", round);
 }
