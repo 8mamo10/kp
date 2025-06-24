8ae183f14d280bfcb75e93ad3a3c161bf0d5db04
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-06-24 23:54:39 +0700

    408B, WIP

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -3,20 +3,7 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: i64,
-        t: [i64; n],
-    }
-
-    let sleep_threshold: f64 = s as f64 + 0.5;
-    let mut last_time: i64 = 0;
-    for i in 0..n {
-        let current_time = t[i];
-        let elapsed_time = (current_time - last_time) as f64;
-        if elapsed_time >= sleep_threshold {
-            println!("No");
-            return;
-        }
-        last_time = current_time;
+        a: [i64; n],
     }
     println!("Yes");
 }
