61d704ddc23e5e63309428a28264ebb0fb7147f1
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-06-28 23:34:53 +0700

    408B

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
@@ -5,5 +7,18 @@ fn main() {
         n: usize,
         a: [i64; n],
     }
-    println!("Yes");
+
+    let mut unique_elements: HashSet<i64> = HashSet::new();
+    for x in a {
+        unique_elements.insert(x);
+    }
+    let mut sorted_unique_elements: Vec<i64> = unique_elements.iter().cloned().collect();
+    sorted_unique_elements.sort();
+    println!("{} ", sorted_unique_elements.len());
+    let output = sorted_unique_elements
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+    println!("{}", output);
 }
