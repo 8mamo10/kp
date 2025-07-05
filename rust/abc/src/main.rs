89f89e065dcb8bae96af6f4d7330c0b27ca47112
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-07-05 23:58:27 +0700

    409B, WIP

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -3,18 +3,7 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        t: String,
-        a: String,
+        a: [i64; n]
     }
-
-    let t_chars: Vec<char> = t.chars().collect();
-    let a_chars: Vec<char> = a.chars().collect();
-
-    for i in 0..n {
-        if t_chars[i] == 'o' && a_chars[i] == 'o' {
-            println!("Yes");
-            return;
-        }
-    }
-    println!("No");
+    println!("yes");
 }
