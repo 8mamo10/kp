81ea79802e35cea84d4efff0b0fa0f0f1439e0ef
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-07-12 08:22:53 +0700

    410B

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -4,10 +4,26 @@ fn main() {
     input! {
         n: usize,
         q: usize,
-        x: [i64; q],
+        x: [usize; q],
     }
+    //print!("{} {} {:?}", n, q, x);
+    let mut box_count = vec![0; n + 1];
     for i in 0..q {
-        print!("{} ", x[i]);
+        if x[i] != 0 {
+            print!("{} ", x[i]);
+            box_count[x[i]] += 1;
+            continue;
+        }
+        let mut min_count = usize::MAX;
+        let mut min_box = usize::MAX;
+        for j in 1..=n {
+            if box_count[j] < min_count {
+                min_count = box_count[j];
+                min_box = j;
+            }
+        }
+        print!("{} ", min_box);
+        box_count[min_box] += 1;
     }
     println!("");
 }
