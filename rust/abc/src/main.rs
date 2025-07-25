4e499b94121631097eae9342f1d625ad46fb0908
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-07-25 19:22:08 +0700

    412A

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -3,18 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        d: [i64; n-1],
     }
 
-    let mut accumatted_distances: Vec<i64> = vec![0; n];
-    for i in 0..(n - 1) {
-        accumatted_distances[i + 1] = accumatted_distances[i] + d[i];
-    }
-    for i in 1..n {
-        for j in (1 + i)..=n {
-            let distance = accumatted_distances[j - 1] - accumatted_distances[i - 1];
-            print!("{} ", distance);
+    let mut count = 0;
+    for _ in 0..n {
+        input! {
+            a: i64,
+            b: i64,
         }
-        println!("")
+        if b > a {
+            count += 1;
+        }
     }
+    println!("{}", count)
 }
