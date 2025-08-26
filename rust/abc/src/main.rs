1838e7f23358f0eca763b1c48d42e08fc807dcf5
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-08-26 23:57:19 +0700

    413A

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -2,21 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        s: String,
-        t: String,
+        n: usize,
+        m: usize,
+        a: [usize; n],
     }
-
-    let uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let s_chars: Vec<char> = s.chars().collect();
-
-    for i in 1..s_chars.len() {
-        if uppers.contains(s_chars[i]) {
-            let a = s_chars[i - 1];
-            if !t.contains(a) {
-                println!("No");
-                return;
-            }
-        }
+    let mut total = 0;
+    for i in 0..n {
+        total += a[i];
+    }
+    if total > m {
+        println!("No");
+        return;
     }
     println!("Yes");
 }
