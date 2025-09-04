503b7f312bf488f895706d754360ef81612ff950
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-09-04 16:51:46 +0700

    415A

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -3,22 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        a: [usize; n],
+        x: usize,
     }
-    let mut result_char = String::new();
-    let mut total_len = 0;
-    for _ in 0..n {
-        input! {
-            c: char,
-            l: usize,
-        }
-        total_len += l;
-        if total_len > 100 {
-            println!("Too Long");
-            return;
-        }
-        for _ in 0..l {
-            result_char.push(c);
-        }
+    if a.contains(&x) {
+        println!("Yes");
+        return;
     }
-    println!("{}", result_char)
+    println!("No");
 }
