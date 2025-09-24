a98def119ab2abea54203c842a36f1103471a365
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-09-24 11:14:42 +0700

    417B

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -1,15 +1,23 @@
 use proconio::input;
-use proconio::marker::Chars;
+//use proconio::marker::Chars;
 
 fn main() {
     input! {
         n: usize,
-        a: usize,
-        b: usize,
-        s: Chars,
+        m: usize,
+        mut a: [i64; n],
+        b: [i64; m],
     }
-    for i in 0..(n - a - b) {
-        print!("{}", s[a + i]);
+    for i in 0..b.len() {
+        for j in 0..a.len() {
+            if a[j] == b[i] {
+                a.remove(j);
+                break;
+            }
+        }
     }
-    println!();
+    for &val in a.iter() {
+        print!("{} ", val);
+    }
+    println!()
 }
