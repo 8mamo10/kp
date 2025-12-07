9d5de77c99f1e95651ad0f9419569ef2b332d65b
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-12-07 17:57:28 +0700

    Add ABC049C

--- cpp/ABS/abc049c.cpp
+++ cpp/ABS/abc049c.cpp
@@ -6,6 +6,36 @@ int main()
     string s;
     cin >> s;
 
-    vector<string> v = {"dreamer", "eraser", "dream", "erase"};
+    while (s.length() >= 5)
+    {
+        string postfix = "";
+        postfix = s.substr(s.length() - 5, 5);
+        if (postfix == "dream" || postfix == "erase")
+        {
+            s.erase(s.length() - 5, 5);
+            continue;
+        }
+        postfix = s.substr(s.length() - 6, 6);
+        if (postfix == "eraser")
+        {
+            s.erase(s.length() - 6, 6);
+            continue;
+        }
+        postfix = s.substr(s.length() - 7, 7);
+        if (postfix == "dreamer")
+        {
+            s.erase(s.length() - 7, 7);
+            continue;
+        }
+        break;
+    }
+    if (s.length() == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
     return 0;
 }
