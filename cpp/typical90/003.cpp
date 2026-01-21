178984f02b0ae8cc257e53a62b91dbeda653c6bc
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2026-01-21 22:16:07 +0700

    Add 003, WIP

---
+++ cpp/typical90/003.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int A[500000];
+int B[500000];
+
+vector<int> graph[500000];
+int dist[500000];
+
+void getdist(int start)
+{
+    for (int i = 1; i < N; i++)
+    {
+        dist[i] = INT_MAX;
+    }
+    queue<int> q;
+    q.push(start);
+    dist[start] = 0;
+    while (!q.empty())
+    {
+        int pos = q.front();
+        q.pop();
+        for (int to : graph[pos])
+        {
+            if (dist[to] == INT_MAX)
+            {
+                dist[to] = dist[pos] + 1;
+                q.push(to);
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> N;
+    for (int i = 1; i < N - 1; i++)
+    {
+        cin >> A[i] >> B[i];
+        graph[A[i]].push_back(B[i]);
+        graph[B[i]].push_back(B[i]);
+    }
+
+    getdist(1);
+    int maxn1 = -1, maxid1 = -1;
+    for (int i = 1; i <= N; i++)
+    {
+        if (maxn1 < dist[i])
+        {
+            maxn1 = dist[i];
+            maxid1 = i;
+        }
+    }
+
+    getdist(maxid1);
+    int maxn2 = -1;
+    for (int i = 1; i <= N; i++)
+    {
+        maxn2 = max(maxn2, dist[i]);
+    }
+    cout << (1 << 29) << endl;
+    cout << maxn2 << endl;
+    cout << maxn2 + 1 << endl;
+    return 0;
+}
