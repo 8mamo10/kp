4bd8d11693d7f26d88dc0c3d9a2022ef38999683
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2026-02-09 14:32:49 +0700

    Add 006, WIP

--- cpp/typical90/006.cpp
+++ cpp/typical90/006.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+int nex[100009][26];
 
 int main()
 {
@@ -7,5 +8,41 @@ int main()
     string S;
     cin >> N >> K >> S;
 
+    for (int i = 0; i < 26; i++)
+    {
+        nex[S.size()][i] = S.size();
+    }
+    for (size_t i = S.size() - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            if ((int)(S[i] - 'a') == j)
+            {
+                nex[i][j] = i;
+            }
+            else
+            {
+                nex[i][j] = nex[i + 1][j];
+            }
+        }
+    }
+    return 0;
+    string ans = "";
+    int current_pos = 0;
+    for (int i = 1; i <= K; i++)
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            int next_pos = nex[current_pos][j];
+            int max_len = S.size() - next_pos - 1 + i;
+            if (max_len >= K)
+            {
+                ans += (char)('a' + j);
+                current_pos = next_pos + 1;
+                break;
+            }
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
